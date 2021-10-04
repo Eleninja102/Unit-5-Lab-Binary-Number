c0a405df59aa82b58d30b74d4132b15cd61ac63a
Coleton Watt <electricninja102@gmail.com>
2021-10-04 08:05:14 -0600

    Initial Commit

---
+++ Unit 5 Lab: Binary Number/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Unit 5 Lab: Binary Number
+//
+//  Created by Coleton Watt on 10/4/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
